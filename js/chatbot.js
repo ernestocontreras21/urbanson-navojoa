b21d958e0577c8ca3fab1f05d6bff5a7a920de3e
ernestocontreras21 <jesusernesto.031121@gmail.cpom>
2026-05-15 22:56:32 -0700

    Chatbot con IA 1.8

--- js/chatbot.js
+++ js/chatbot.js
@@ -190,7 +190,7 @@
     `;
 
         const response = await fetch(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
             {
                 method: 'POST',
                 headers: {
